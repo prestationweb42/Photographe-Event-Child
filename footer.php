3c914514a9768c52511dc89a830be58324fd3649
prestationweb42 <dani.vent@orange.fr>
2023-07-28 11:43:56 +0200

    clean footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
 <footer id="" class="">
-    <?php get_template_part('template-parts/newsletter'); ?>
-    <!-- Assignation du menu pied de page -->
     <?php wp_nav_menu(
         array(
             'theme_location' => 'footer',
@@ -8,7 +6,10 @@
             'menu_class' => 'site__header__menu', // ma classe personnalisée 
         )
     ); ?>
-</footer><!-- id/class -->
+    <!-- Mention texte -->
+    <p class="footer_text">Tous droits réxervés</p>
+</footer>
+<!-- id/class -->
 <?php wp_footer(); ?>
 </body>
 
