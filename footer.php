1b388b38070790a52c451a0f9f128be1a8cd09eb
prestationweb42 <dani.vent@orange.fr>
2023-07-29 13:51:35 +0200

    setting footer

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
-<footer id="" class="">
+<footer id="footer_menu" class="footer_menu">
     <?php wp_nav_menu(
         array(
             'theme_location' => 'footer',
             'container' => 'ul', // afin d'éviter d'avoir une div autour 
-            'menu_class' => 'site__header__menu', // ma classe personnalisée 
+            'menu_class' => 'site__footer__menu', // ma classe personnalisée 
         )
     ); ?>
     <!-- Mention texte -->
-    <p class="footer_text">Tous droits réxervés</p>
+    <p class="footer_text">Tous droits réservés</p>
 </footer>
 <!-- id/class -->
 <?php wp_footer(); ?>
