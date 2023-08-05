1fc3489bff02b9250a296b52e7d06d8cf4227361
prestationweb42 <dani.vent@orange.fr>
2023-08-05 16:20:26 +0200

    add link contact single page open modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Animation header Btn Menu Burger
+// **** Animation header Btn Menu Burger ****
 const menuBurger = document.querySelector(".btn_menu");
 const menuMobile = document.querySelector(".header_mobile");
 const headerTitleLink = document.querySelector("#header_title_link");
@@ -13,21 +13,27 @@ const menuContactHeader = document.querySelector(
     ".header__menu__desktop .menu-item-23 a"
 );
 const popUpOverlay = document.querySelector(".popup_overlay");
-const btnClosePopUp = document.querySelector(".popup-close");
 // Open Modale Form
 menuContactHeader.addEventListener("click", () => {
     popUpOverlay.classList.toggle("is-open");
 });
+//  Link Contact Single Page Open Modal ****
+const contactSinglePageModal = document.querySelector(".post_contact_link");
+// Open Modale Form
+contactSinglePageModal.addEventListener("click", () => {
+    console.log("ca marche");
+    popUpOverlay.classList.toggle("is-open");
+});
 // Close Modale Form
+const btnClosePopUp = document.querySelector(".popup-close");
 btnClosePopUp.addEventListener("click", () => {
     popUpOverlay.classList.remove("is-open");
 });
-// Link Contact Mobile Popup
+// **** Link Contact Mobile Popup ****
 const menuContactPopUp = document.querySelector(
     ".header__menu__mobile .menu-item-23 a"
 );
 menuContactPopUp.addEventListener("click", () => {
-    console.log("ca marche");
     menuBurger.classList.toggle("is-active");
     menuMobile.classList.toggle("is-active");
     popUpOverlay.classList.toggle("is-open");
