a3d7d63b6aaafb71035c938d771eb1549bfa828d
prestationweb42 <dani.vent@orange.fr>
2023-08-11 15:06:49 +0200

    add function Toutes les photos

--- js/j-query.js
+++ js/j-query.js
@@ -1,16 +1,36 @@
-var page = 2;
+var front_page = 2;
 jQuery(function ($) {
     $("body").on("click", ".btn_load_more", function () {
         var data = {
             action: "load_posts_by_ajax",
-            page: page,
+            page: front_page,
             security: photo.security,
         };
 
         $.post(photo.ajaxurl, data, function (response) {
             if ($.trim(response) != "") {
                 $(".section_post_imgs_container").append(response);
-                page++;
+                front_page++;
+            } else {
+                $(".btn_load_more").hide();
+            }
+        });
+    });
+});
+// single-page.php
+var single_page = 2;
+jQuery(function ($) {
+    $("body").on("click", ".btn_load_all_imgs", function () {
+        var data = {
+            action: "load_posts_by_ajax",
+            page: single_page,
+            security: photo.security,
+        };
+
+        $.post(photo.ajaxurl, data, function (response) {
+            if ($.trim(response) != "") {
+                $(".post_other_imgs_container").append(response);
+                single_page++;
             } else {
                 $(".btn_load_more").hide();
             }
