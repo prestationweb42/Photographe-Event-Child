c4ddea1c524cf2bfdeb0ecf28c50501652df072c
prestationweb42 <dani.vent@orange.fr>
2023-08-11 11:03:12 +0200

    add function ajax load more

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ function child_enqueue_styles()
     wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/sass/style.css', array(), 100);
     wp_enqueue_style('font-awesome-bundle-script', 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200', array(), false, 'all');
     wp_enqueue_script('child-script', get_stylesheet_directory_uri() . '/js/main.js', array(), '0.0.1', true);
-    wp_enqueue_script('jquery-script', get_stylesheet_directory_uri() . '/js/j-query.js', array('jquery'), null, true);
+    // wp_enqueue_script('jquery-script', get_stylesheet_directory_uri() . '/js/j-query.js', array('jquery'), null, true);
 }
 /**
  * include custom jQuery
@@ -42,9 +42,58 @@ function shapeSpace_include_custom_jquery()
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js', array(), null, true);
 }
+/**
+ * Load more Ajax
+ */
+function photo_scripts()
+{
+    // Register the script
+    wp_enqueue_script('jquery-script', get_stylesheet_directory_uri() . '/js/j-query.js', array('jquery'), false, true);
 
+    // Localize the script with new data
+    $script_data_array = array(
+        'ajaxurl' => admin_url('admin-ajax.php'),
+        'security' => wp_create_nonce('load_more_posts'),
+    );
+    wp_localize_script('jquery-script', 'photo', $script_data_array);
+
+    // Enqueued script with localized data.
+    wp_enqueue_script('jquery-script');
+}
+/**
+ * Load post Ajax
+ */
+function load_posts_by_ajax_callback()
+{
+    check_ajax_referer('load_more_posts', 'security');
+    $args = array(
+        'post_type' => 'photo',
+        'post_status' => 'publish',
+        'posts_per_page' => '2',
+        'paged' => $_POST['page'],
+    );
+    $blog_posts = new WP_Query($args);
+?>
+<?php if ($blog_posts->have_posts()) : ?>
+<?php while ($blog_posts->have_posts()) : $blog_posts->the_post(); ?>
+<div class="post_img">
+    <?php $image_id = get_field('image'); // On récupère cette fois l'ID
+                if ($image_id) {
+                    echo wp_get_attachment_image($image_id, 'medium-large');
+                } ?>
+</div>
+<?php endwhile; ?>
+<?php wp_reset_postdata(); ?>
+<?php endif; ?>
+<?php
+    wp_die();
+}
+//
+add_action('wp_enqueue_scripts', 'photo_scripts');
 add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');
 add_action('wp_enqueue_scripts', 'child_enqueue_styles');
+add_action('wp_ajax_load_posts_by_ajax', 'load_posts_by_ajax_callback');
+add_action('wp_ajax_nopriv_load_posts_by_ajax', 'load_posts_by_ajax_callback');
 
 // change HEADER menu CONTACT Link
 add_filter('wp_nav_menu_items', 'add_header_link', 10, 2);
