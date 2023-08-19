dc8dc637b8161c087def1dad44cdb81d3d5d9488
prestationweb42 <dani.vent@orange.fr>
2023-08-19 15:19:27 +0200

    test send value filter

--- js/j-query.js
+++ js/j-query.js
@@ -37,3 +37,42 @@ jQuery(function ($) {
         });
     });
 });
+//
+$(document).ready(function () {
+    $(".span_title_filter").on("change", function () {
+        // var selectedValue = $('select[name="selection"]').val();
+        // var selectedValue = $(".title_filter_box").val();
+        var selectedValue = $(".span_title_filter").text();
+        console.log(selectedValue);
+
+        // Envoyer la valeur à PHP via une requête AJAX ou autre
+        $.ajax({
+            type: "POST",
+            url: "/",
+            data: { selectedValue: selectedValue },
+            success: function (response) {
+                // Traiter la réponse du serveur si nécessaire
+                console.log("toto");
+            },
+        });
+    });
+});
+// chatgpt
+$(document).ready(function () {
+    // Sélectionner l'élément que vous souhaitez observer
+    var target = document.querySelector(".span_title_filter");
+
+    // Créer une instance de MutationObserver
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            var contenuDiv = $(mutation.target).text(); // Récupérer le contenu textuel de la div modifiée
+            console.log(contenuDiv); // Afficher le contenu dans la console ou effectuer d'autres actions
+        });
+    });
+
+    // Options de l'observateur
+    var config = { childList: true, subtree: true };
+
+    // Commencer l'observation de la cible avec les options
+    observer.observe(target, config);
+});
