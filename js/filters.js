192e7cff937e667fa13e9599a493e965cfe796f3
prestationweb42 <dani.vent@orange.fr>
2023-08-26 09:59:22 +0200

    change id section_result_filtered

--- js/filters.js
+++ js/filters.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
         xhr.open("POST", photo.ajaxurl, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                document.getElementById("result_filtered").innerHTML =
+                document.getElementById("section_result_filtered").innerHTML =
                     xhr.responseText;
 
                 // Appel de la fonction pour vérifier les filtres après avoir chargé les résultats
