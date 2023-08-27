e1c34462d96c3db4406de10035e4bc2b31be3f81
prestationweb42 <dani.vent@orange.fr>
2023-08-27 11:59:57 +0200

    send function load ajax -> file load_ajax_function

--- functions.php
+++ functions.php
@@ -43,30 +43,11 @@ function child_enqueue_styles()
     wp_enqueue_script('child-script', get_stylesheet_directory_uri() . '/js/main.js', array(), '0.0.1', true);
 }
 
-/**
- * Load more Ajax
- */
-function photo_scripts()
-{
-    // Register the script
-    wp_enqueue_script('jquery-script', get_stylesheet_directory_uri() . '/js/j-query.js', array('jquery'), false, true);
 
-    // Localize the script with new data
-    $script_data_array = array(
-        'ajaxurl' => admin_url('admin-ajax.php'),
-        'security' => wp_create_nonce('load_more_posts'),
-    );
-    wp_localize_script('jquery-script', 'photo', $script_data_array);
-
-    // Enqueued script with localized data.
-    wp_enqueue_script('jquery-script');
-}
-
-//
-add_action('wp_enqueue_scripts', 'photo_scripts');
 add_action('wp_enqueue_scripts', 'child_enqueue_styles');
 //
 require_once get_template_directory() . '/inc/load_jquery_function.php';
+require_once get_template_directory() . '/inc/load_ajax_function.php';
 require_once get_template_directory() . '/inc/load_all_imgs_function.php';
 require_once get_template_directory() . '/inc/load_more_imgs_function.php';
 require_once get_template_directory() . '/inc/filtered_function.php';
