eb00779e70f968a510cbd83b8a64978165d4269b
prestationweb42 <dani.vent@orange.fr>
2023-08-27 12:02:25 +0200

    new file load_ajax_function

---
+++ inc/load_ajax_function.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Load Ajax -> MORE and ALL Imgs
+ */
+function ajax_scripts()
+{
+    // Register the script
+    wp_enqueue_script('jquery-script', get_stylesheet_directory_uri() . '/js/j-query.js', array('jquery'), false, true);
+
+    // Localize the script with new data
+    $script_data_array = array(
+        'ajaxurl' => admin_url('admin-ajax.php'),
+        'security' => wp_create_nonce('load_more_posts'),
+    );
+    wp_localize_script('jquery-script', 'photo', $script_data_array);
+
+    // Enqueued script with localized data.
+    wp_enqueue_script('jquery-script');
+}
+
+//
+add_action('wp_enqueue_scripts', 'ajax_scripts');
