0f2e92b30b4bc9284ce035dcd5b0958060f8bddb
prestationweb42 <dani.vent@orange.fr>
2023-08-27 11:16:49 +0200

    new file load_jquey_function

---
+++ inc/load_jquery_function.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Doculent include custom jQuery
+ */
+function shapeSpace_include_custom_jquery()
+{
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');
