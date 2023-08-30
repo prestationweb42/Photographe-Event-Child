fe6bf5f3df9be081ca11954c54b2cf0a82c02262
prestationweb42 <dani.vent@orange.fr>
2023-08-30 15:08:18 +0200

    change url imgs -> img

--- header.php
+++ header.php
@@ -16,7 +16,8 @@
             <!-- Assignation du menu principal -->
             <!-- Logo avec lien sur accueil -->
             <a href="<?php echo home_url('/'); ?>" class="header_logo_link">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/logo.png" class="header_logo_img" alt="Logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="header_logo_img"
+                    alt="Logo">
             </a>
             <?php
             wp_nav_menu(
