6b38afa8d368768c9a6519f35a1ba9b4d25fa32d
prestationweb42 <dani.vent@orange.fr>
2023-08-30 11:49:41 +0200

    require lightbox_script_function.php

--- functions.php
+++ functions.php
@@ -41,3 +41,4 @@ require_once get_template_directory() . '/inc/load_ajax_function.php';
 require_once get_template_directory() . '/inc/load_all_imgs_function.php';
 require_once get_template_directory() . '/inc/load_more_imgs_function.php';
 require_once get_template_directory() . '/inc/filtered_function.php';
+require_once get_template_directory() . '/inc/lightbox_script_function.php';
