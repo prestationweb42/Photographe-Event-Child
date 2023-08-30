0eaaba7e981dc6389b8bb80cde6eb139c42a481f
prestationweb42 <dani.vent@orange.fr>
2023-08-30 15:14:05 +0200

    comment my lightBoxOpen

--- js/main.js
+++ js/main.js
@@ -62,13 +62,13 @@ document.addEventListener("keydown", event => {
 // Class Lightbox Overlay
 const lightboxOverlay = document.querySelector(".lightbox_overlay");
 // Class Btn Open Lightbox
-const lightboxOpen = document.querySelectorAll(".icon_fullscreen");
+// const lightboxOpen = document.querySelectorAll(".icon_fullscreen");
 // Function Open Lightbox
-lightboxOpen.forEach(el => {
-    el.addEventListener("click", () => {
-        lightboxOverlay.classList.add("active-lightbox");
-    });
-});
+// lightboxOpen.forEach(el => {
+//     el.addEventListener("click", () => {
+//         lightboxOverlay.classList.add("active-lightbox");
+//     });
+// });
 // Close Lightbox - Btn Class
 const closeLightbox = document.querySelector(".close_lightbox");
 // Function Close Lightbox
