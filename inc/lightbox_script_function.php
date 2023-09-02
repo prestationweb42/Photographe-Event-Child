ce1b81585c4fb61a6cb8c504188b13b2ecba15da
prestationweb42 <dani.vent@orange.fr>
2023-09-02 09:02:47 +0200

    delete enqueue script lightbox.css

--- inc/lightbox_script_function.php
+++ inc/lightbox_script_function.php
@@ -5,7 +5,6 @@
  */
 function lightbox_enqueue_script()
 {
-    wp_enqueue_style('lightbox-style', get_stylesheet_directory_uri() . '/sass/lightbox.css', array(), 100);
     wp_enqueue_script('lightbox-script', get_stylesheet_directory_uri() . '/js/lightbox.js', array('jquery'), '0.0.1', true);
 }
 add_action('wp_enqueue_scripts', 'lightbox_enqueue_script');
