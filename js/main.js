1ac728aa4fc2959e5e0fbd613792b0dd0482ca11
prestationweb42 <dani.vent@orange.fr>
2023-09-02 08:58:02 +0200

    delete code lightbox animation

--- js/main.js
+++ js/main.js
@@ -57,25 +57,6 @@ document.addEventListener("keydown", event => {
     }
 });
 /**
- * Animation Lightbox Modale
- */
-// Class Lightbox Overlay
-const lightboxOverlay = document.querySelector(".lightbox_overlay");
-// Class Btn Open Lightbox
-// const lightboxOpen = document.querySelectorAll(".icon_fullscreen");
-// Function Open Lightbox
-// lightboxOpen.forEach(el => {
-//     el.addEventListener("click", () => {
-//         lightboxOverlay.classList.add("active-lightbox");
-//     });
-// });
-// Close Lightbox - Btn Class
-const closeLightbox = document.querySelector(".close_lightbox");
-// Function Close Lightbox
-closeLightbox.addEventListener("click", () => {
-    lightboxOverlay.classList.remove("active-lightbox");
-});
-/**
  * Animation Filter Categories
  */
 const titleBoxCategory = document.getElementById("title_box_category");
