307b94d5dc8936d442d8a68318030997ca4bef77
prestationweb42 <dani.vent@orange.fr>
2023-09-07 16:59:42 +0200

    replace directori uri imgs lightbox

--- template-parts/lightbox.php
+++ template-parts/lightbox.php
@@ -1,22 +1,19 @@
 <div class="lightbox_overlay">
     <div class="lightbox_wrapper">
         <div class="before_arrow">
-            <img src="http://localhost:8888/PhotographeEvent/wp-content/themes/photographe-event/assets/img/before.svg"
-                alt="icone précédent">
+            <img src="<?php echo get_template_directory_uri() . '/assets/img/before.svg' ?>" alt="icone précédent">
         </div>
         <div class="before_chevron">
             &#10216;
         </div>
         <div class="after_arrow">
-            <img src="http://localhost:8888/PhotographeEvent/wp-content/themes/photographe-event/assets/img/after.svg"
-                alt="icone suivant">
+            <img src="<?php echo get_template_directory_uri() . '/assets/img/after.svg' ?>" alt=" icone suivant">
         </div>
         <div class="after_chevron">
             &#10217;
         </div>
         <div class="close_lightbox">
-            <img src="http://localhost:8888/PhotographeEvent/wp-content/themes/photographe-event/assets/img/close.png"
-                alt="icone suivant">
+            <img src="<?php echo get_template_directory_uri() . '/assets/img/close.png' ?>" alt="icone fermer">
         </div>
         <div class="lightbox_img">
             <?php $args = array(
@@ -26,9 +23,10 @@
 
             $lightbox_query = new WP_Query($args); ?>
             <?php if ($lightbox_query->have_posts()) : while ($lightbox_query->have_posts()) : $lightbox_query->the_post(); ?>
-            <div class="lightbox_text lightbox_reference"><?php the_field('reference'); ?></div>
-            <div class="lightbox_text lightbox_categorie"><?php the_field('categories'); ?></div>
-            <?php $image_id = get_field('image'); 
+                    <div class=" lightbox_text lightbox_reference"><?php the_field('reference'); ?>
+                    </div>
+                    <div class="lightbox_text lightbox_categorie"><?php the_field('categories'); ?></div>
+                    <?php $image_id = get_field('image');
                     if ($image_id) {
                         echo wp_get_attachment_image($image_id, 'large');
                     } ?>
