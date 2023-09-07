6ae9578cf8a0a5ae0a6e7a4b5b82843eb957dbf4
prestationweb42 <dani.vent@orange.fr>
2023-09-07 11:32:48 +0200

    add code enqueue style / script

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Enqueue Child Scripts.
+ */
+function child_enqueue_script()
+{
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/sass/style.css', array(), 100);
+    wp_enqueue_script('child-script', get_stylesheet_directory_uri() . '/js/main.js', array(), '0.0.1', true);
+}
+add_action('wp_enqueue_scripts', 'child_enqueue_script');
+
 // Ajouter 2 emplacements de menus
 register_nav_menus(array(
     'main' => 'Menu Principal',
