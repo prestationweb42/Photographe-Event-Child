47076a118d6723bcfd2f45c2a3271ad11f31f7d1
prestationweb42 <dani.vent@orange.fr>
2023-09-08 17:43:02 +0200

    edit div receive infos

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,18 +2,49 @@
  * Animation Lightbox Modale
  */
 // Class Lightbox Overlay
-const lightboxOverlay = document.querySelector(".lightbox_overlay");
+let lightboxOverlay = document.querySelector(".lightbox_overlay");
 // Class Btn Open Lightbox
-const lightboxOpen = document.querySelectorAll(".icon_fullscreen");
-// Function Open Lightbox
-lightboxOpen.forEach(el => {
-    el.addEventListener("click", () => {
-        lightboxOverlay.classList.add("active-lightbox");
-    });
-});
+let lightboxOpen = document.querySelectorAll(".icon_fullscreen");
 // Close Lightbox - Btn Class
-const closeLightbox = document.querySelector(".close_lightbox");
+let closeLightbox = document.querySelector(".close_lightbox");
 // Function Close Lightbox
 closeLightbox.addEventListener("click", () => {
     lightboxOverlay.classList.remove("active-lightbox");
+});
+// Function Open Lightbox
+lightboxOpen.forEach(function (div, index) {
+    div.addEventListener("click", e => {
+        e.preventDefault();
+        lightboxOverlay.classList.add("active-lightbox");
+        //
+        let imageTitre = div.getAttribute("data-title");
+        console.log(imageTitre);
+        // img
+        let imageSrc = div.getAttribute("data-image");
+        if (imageSrc) {
+            let lightboxImage =
+                lightboxOverlay.querySelector(".lightbox__image");
+            if (lightboxImage) {
+                lightboxImage.setAttribute("src", imageSrc);
+            } else {
+                console.log("Élément .lightbox__image introuvable.");
+            }
+        } else {
+            console.log("Attribut data-image introuvable ou vide.");
+        }
+        // categorie
+        let imagecateg = div.getAttribute("data-categorie");
+        console.log(imagecateg);
+        let lightboxcateg = lightboxOverlay.querySelector(
+            ".lightbox_categorie"
+        );
+        lightboxcateg.textContent = imagecateg;
+        // reference
+        let imageRef = div.getAttribute("data-reference");
+        console.log(imageRef);
+        let lightboxReference = lightboxOverlay.querySelector(
+            ".lightbox_reference"
+        );
+        lightboxReference.textContent = imageRef;
+    });
 });
