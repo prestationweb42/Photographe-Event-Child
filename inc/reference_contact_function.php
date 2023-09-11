70089ddc1b20870e195989a1a7153cedfe82eade
prestationweb42 <dani.vent@orange.fr>
2023-09-11 11:01:04 +0200

    create reference_contact_function.php

---
+++ inc/reference_contact_function.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Get Reference Contact Form 
+ */
+add_filter('wpcf7_form_tag', function ($tag) {
+    $name = is_object($tag) ? $tag->name : $tag['name'];
+
+    if ('your-reference' === $name) {
+        global $post;
+        $reference_value = get_post_meta($post->ID, 'reference', true); // Remplacez 'reference' par le bon meta key.
+
+        if ($reference_value) {
+            if (is_object($tag)) {
+                $tag->values = array($reference_value);
+            } elseif (is_array($tag)) {
+                $tag['values'] = array($reference_value);
+            }
+        }
+    }
+    return $tag;
+});
