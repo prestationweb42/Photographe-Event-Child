cff35f91906407390c2c139b9585f08a60e5ea20
prestationweb42 <dani.vent@orange.fr>
2023-09-16 10:45:54 +0200

    template-part block post-card.php

--- front-page.php
+++ front-page.php
@@ -31,28 +31,9 @@
             // Execution loop of wp-query
             while ($query->have_posts()) : $query->the_post();
                 $post_url = get_permalink();
-                // $references = get_field('reference');
             ?>
-    <div class="post_img">
-        <!-- Overlay Img -->
-        <div class="post_img_overlay">
-            <div class="text_category"><?php the_field('categories'); ?></div>
-            <div class="text_reference"><?php the_field('reference'); ?></div>
-            <div class="icon_eye">
-                <a href="<?php echo $post_url; ?>" class="lightbox_trigger">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/img/icon-eye.svg' ?>">
-                </a>
-            </div>
-            <div class="icon_fullscreen" data-title="<?php the_title(); ?>"
-                data-image="<?php echo esc_attr(get_the_post_thumbnail_url(get_the_ID())); ?>"
-                data-reference="<?php the_field('reference'); ?>" data-categorie="<?php the_field('categories') ?>">
-                <img src="
-                    <?php echo get_template_directory_uri() . '/assets/img/Icon_fullscreen.png' ?>">
-            </div>
-        </div>
-        <!-- Overlay Img -->
-        <?php get_template_part('template-parts/post-img'); ?>
-    </div>
+    <!-- Post Card -->
+    <?php get_template_part('template-parts/post-card'); ?>
 
     <?php endwhile;
             wp_reset_postdata() ?>
