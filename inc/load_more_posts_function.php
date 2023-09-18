fc5d725ab65d23b47d7e76619ddc9fdbabdc9fda
prestationweb42 <dani.vent@orange.fr>
2023-09-18 17:58:50 +0200

    update function load more posts

--- inc/load_more_posts_function.php
+++ inc/load_more_posts_function.php
@@ -6,23 +6,25 @@
 function load_more_imgs()
 {
     check_ajax_referer('load_more_posts', 'security');
+
     $args = array(
         'post_type' => 'photo',
         'post_status' => 'publish',
-        'posts_per_page' => '2',
+        'posts_per_page' => 2,
         'paged' => $_POST['page'],
+        'post__not_in' => explode(',', $_POST['exclude']),
     );
     $query_more_imgs = new WP_Query($args);
 ?>
-    <?php if ($query_more_imgs->have_posts()) : ?>
-        <?php while ($query_more_imgs->have_posts()) : $query_more_imgs->the_post();
+<?php if ($query_more_imgs->have_posts()) : ?>
+<?php while ($query_more_imgs->have_posts()) : $query_more_imgs->the_post();
             $post_url = get_permalink();
         ?>
-            <!-- Template Post Card -->
-            <?php get_template_part('template-parts/post-card'); ?>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
+<!-- Template Post Card -->
+<?php get_template_part('template-parts/post-card'); ?>
+<?php endwhile; ?>
+<?php wp_reset_postdata(); ?>
+<?php endif; ?>
 <?php
     wp_die();
 }
