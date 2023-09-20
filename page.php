200df2b9403731d49a014fa7e68a32a9d7971c3a
prestationweb42 <dani.vent@orange.fr>
2023-09-20 07:21:13 +0200

    update h1 en construction

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<h1><?php the_title(); ?></h1>
+<h1>Page : <?php the_title(); ?> en construction</h1>
 
 <?php the_content(); ?>
 
