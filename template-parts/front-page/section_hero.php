06ac2b17ae54e23dbb40749428f8b04101e0bfc6
prestationweb42 <dani.vent@orange.fr>
2023-09-25 07:38:03 +0200

    rename post-img -> photo_img

--- template-parts/front-page/section_hero.php
+++ template-parts/front-page/section_hero.php
@@ -28,16 +28,15 @@
     // Boucle d'execution de wp query
     while ($query->have_posts()) : $query->the_post();
     ?>
-    <div class="hero_img">
-        <?php get_template_part('template-parts/post-img'); ?>
-    </div>
+        <div class="hero_img">
+            <?php get_template_part('template-parts/photo_img'); ?>
+        </div>
     <?php endwhile;
     wp_reset_postdata() ?>
 
     <h1 class="hero_section_title">
         <svg viewbox="0 0 10 2">
-            <text x="5" y="1" text-anchor="middle" font-size="0.7" fill="none" stroke-width=".02" stroke="#fff"
-                font-family="space mono" font-style="italic" font-weight=900 text-transform="uppercase">PHOTOGRAPHE
+            <text x="5" y="1" text-anchor="middle" font-size="0.7" fill="none" stroke-width=".02" stroke="#fff" font-family="space mono" font-style="italic" font-weight=900 text-transform="uppercase">PHOTOGRAPHE
                 EVENT
             </text>
         </svg>
--- template-parts/photo_block.php
+++ template-parts/photo_block.php
@@ -9,11 +9,13 @@
                 <img src="<?php echo get_template_directory_uri() . '/assets/img/icon-eye.svg' ?>">
             </a>
         </div>
-        <div class="icon_fullscreen" data-title="<?php the_title(); ?>" data-image="<?php echo esc_attr(get_the_post_thumbnail_url(get_the_ID())); ?>" data-reference="<?php the_field('reference'); ?>" data-categorie="<?php the_field('categories') ?>">
+        <div class="icon_fullscreen" data-title="<?php the_title(); ?>"
+            data-image="<?php echo esc_attr(get_the_post_thumbnail_url(get_the_ID())); ?>"
+            data-reference="<?php the_field('reference'); ?>" data-categorie="<?php the_field('categories') ?>">
             <img src="
                     <?php echo get_template_directory_uri() . '/assets/img/Icon_fullscreen.png' ?>">
         </div>
     </div>
     <!-- Template Part post Img -->
-    <?php get_template_part('template-parts/post-img'); ?>
+    <?php get_template_part('template-parts/photo_img'); ?>
 </div>
